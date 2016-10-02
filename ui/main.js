0a78007bf0e096e840e9a65d884e9d736231b17b
AnujaSreejithbabu <anujasreejithbabu@gmail.com>
2016-10-02 19:45:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
     
-    //Make a request to the counter endpoint
+    //Create a request object
     var request = new XMLHttpRequest();
     
     //capture the response and store it in a variable
@@ -18,4 +18,8 @@ button.onclick = function() {
         }
        //Not done yet 
     };
+    
+    //Make the request
+    request.open('GET', 'http://anujasreejithbabu.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
