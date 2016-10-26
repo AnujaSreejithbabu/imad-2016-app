348bb74be62a030dd8cce4e5f0ccc6533f8cff79
AnujaSreejithbabu <anujasreejithbabu@gmail.com>
2016-10-26 23:37:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,21 @@
+// Move the image
+var img = document.getElementById('madi');
+
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+    
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight, 50);
+    
+    
+    //img.style.marginLeft = '100px';        to Jump right
+};
+
+
+
 //counter code
 var button = document.getElementById('counter');
 var counter = 0;
