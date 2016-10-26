150fe29daac8ca1209288724cead6abd355d8c35
AnujaSreejithbabu <anujasreejithbabu@gmail.com>
2016-10-27 00:20:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,7 +52,7 @@ submit.onclick = function () {
    
    //Capture the response and store it in a variable
    request.onreadystatechange = function () {
-        if(request.readystate === XMLHttpRequest.DONE) {
+        if (request.readystate === XMLHttpRequest.DONE) {
              //Take some action
             if (request.status === 200) {
                 //Capture a list of names and render it as a list
