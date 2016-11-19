f98cf21f70fe4da2c0cab4ad440b51eed7e00bc8
AnujaSreejithbabu <anujasreejithbabu@gmail.com>
2016-11-19 20:17:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,6 +15,47 @@ img.onclick = function() {
 };
 
 
+//submit username/password to login
+
+
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+   //Create a request object
+   var request = new XMLHttpRequest();
+   
+   //Capture the response and store it in a variable
+   request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+             //Take some action
+            if (request.status === 200) {
+                //Capture a list of names and render it as a list
+	console.log('user logged in');
+	alert('logged in successfully');
+            } else if (request.status === 403) {
+	alert('Username/password is incorrect');
+} else if (request.status === 500) { 
+alert('something went wrong on the server');
+}
+        }
+    };
+    
+    //make the request
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+  console.log(username);   //for debugging purpose
+console.log(password);
+
+    request.open('POST', 'http://anujasreejithbabu.imad.hasura-app.io/login', true);
+   request.setRequestHeader('Content-Type', 'application/json'); 
+   request.send(JSON.stringify ({username: username, password: password}));
+};
+
+
+
+
+
+
+
 
 //counter code
 var button = document.getElementById('counter');
