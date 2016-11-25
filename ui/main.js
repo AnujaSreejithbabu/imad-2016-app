a8a2e88e7d39069da2b61fe77f6bd31617dbdfc4
AnujaSreejithbabu <anujasreejithbabu@gmail.com>
2016-11-25 23:39:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -112,7 +112,7 @@ submit1.onclick = function () {
     //make the request
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
-    request.open('GET', 'http://anujasreejithbabu.imad.hasura-app.io/submit-name1?name=' + name, true);
+    request.open('GET', 'http://anujasreejithbabu.imad.hasura-app.io/submit-name?name=' + name, true);
     request.send(null);
 };
 
