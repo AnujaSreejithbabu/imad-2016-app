91215769f13d804cc67b8678671fd1c3a60e27a6
AnujaSreejithbabu <anujasreejithbabu@gmail.com>
2016-11-26 01:06:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ app.get('/counter', function(req, res) {
 });
 
 var names =[];
-app.get('/submit-name1', function(req,res) { // URL: /submit-name1?name=xxxx
+app.get('/submit-name', function(req,res) { // URL: /submit-name?name=xxxx
     //Get the name from the request
     var name = req.query.name;
     
